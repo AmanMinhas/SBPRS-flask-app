05a2cc34b656e7750dad9a158ae0a713afecf081
AmanMinhas <amandeepSinghMinhas@gmail.com>
2021-10-11 00:02:16 +0530

    Update Design and Save new pickel files

--- model.py
+++ model.py
@@ -15,33 +15,36 @@ class RecommendationSystem():
 
     # load models
     recommendation_pickle_filepath = './models/user_final_rating.pickle'
-    sentiment_analysis_mdel_filepath = './models/lr_sentiment_analysis_model.pickle'
+    sentiment_analysis_mdel_filepath = './models/final_sentiment_analysis_model.pickle'
     vectorizer_pickle_filepath = './models/tfidf_vectorizer.pickle'
+    product_sentiment_score_df_filepath = './models/product_sentiment_score_df.pickle'
 
     self.recommendation_system = pickle.load(open(recommendation_pickle_filepath, 'rb'))
     self.sentiment_analysis_model = pickle.load(open(sentiment_analysis_mdel_filepath, 'rb'))
     self.vectorizer_pickle = pickle.load(open(vectorizer_pickle_filepath, 'rb'))
+    self.product_sentiment_score_df = pickle.load(open(product_sentiment_score_df_filepath, 'rb'))
 
     # Create a lemmatizer
     self.lemmatizer = WordNetLemmatizer()
 
-  def document_preprocess(self, document):
-    # convert to lowercase 
-    document = document.lower()
-    # tokenize into words
-    words = word_tokenize(document)
-    # replace "." with ". ". Add space after drop so that when puntuations are removed. 2 words are not grouped together.
-    # TODO
-    # replace puntuations with space
-    for i in range (0, len(words)):
-      word = words[i]
-      words[i] = "".join(char if char not in set(string.punctuation) else " " for char in word)
-    # remove stop words
-    words = [word for word in words if word not in stopwords.words("english")]
-    # lemmatize 
-    words = [self.lemmatizer.lemmatize(word) for word in words]
-    document = " ".join(words)
-    return document
+  def get_top_20_recommendations_2(self, username):
+    """
+      This function uses the product_sentiment_score_df dataframe which was precreated from the notebook and uses it for sorting recommendations
+    """
+    # 1. Get top 20 recommendations
+    top_20_product_recommendations = self.recommendation_system.loc[username].sort_values(ascending=False)[0:20]
+    top_20_product_recommendations_list = top_20_product_recommendations.index.tolist()
+    print('top_20_product_recommendations_list :')
+    print(top_20_product_recommendations_list)
+
+    # 2. Sort product list based on sentiment score 
+    product_sentiment_score_df = self.product_sentiment_score_df
+    top_20_products_sorted_df = product_sentiment_score_df[product_sentiment_score_df['product'].isin(top_20_product_recommendations_list)].sort_values(by="sentiment_score", ascending=False)
+    top_20_products_sorted_list = top_20_products_sorted_df['product'].tolist()
+    print('top_20_products_sorted_list')
+    print(top_20_products_sorted_list)
+
+    return top_20_products_sorted_list
 
   def get_top_20_recommendations(self, username):
     # 1. Get top 20 recommendations
@@ -51,17 +54,19 @@ class RecommendationSystem():
     print(top_20_product_recommendations_list)
     
     # 2. Preprocess all reviews for top 20 products
-    print("Proprocessing documents")
+    # print("Proprocessing documents")
     top_products_reviews = self.og_ratings_df[self.og_ratings_df.name.isin(top_20_product_recommendations_list)][['name', 'reviews_title', 'reviews_text']]
-    top_products_reviews['preprocessed_review'] = top_products_reviews.reviews_text.apply(self.document_preprocess)
-    print("Proprocessing completed")
+    # top_products_reviews['preprocessed_review'] = top_products_reviews.reviews_text.apply(self.document_preprocess)
+    # print("Proprocessing completed")
 
     # 3. Pass the preprocessed reviews to verctorizer
     print("Vector Transforming")
-    tfidf_model = self.vectorizer_pickle.transform(top_products_reviews['preprocessed_review'])
+    # tfidf_model = self.vectorizer_pickle.transform(top_products_reviews['preprocessed_review'])
+    tfidf_model = self.vectorizer_pickle.transform(top_products_reviews['reviews_text'])
     tfidf_df = pd.DataFrame(tfidf_model.toarray(), columns = self.vectorizer_pickle.get_feature_names())
+    # tfidf_df = pd.DataFrame(tfidf_model.toarray(), columns = self.vectorizer_pickle.get_feature_names_out())
 
-    cutoff = 0.42
+    cutoff = 0.45
     print("Making Prediction")
     top_products_reviews["pos_sentiment_prob"] = self.sentiment_analysis_model.predict_proba(tfidf_df)[:, 1]
     top_products_reviews["sentiment_pred"] = top_products_reviews['pos_sentiment_prob'].map(lambda x: 1 if x >= cutoff else 0)
@@ -70,4 +75,4 @@ class RecommendationSystem():
     print("top_products_list :")
     print(top_products_list)
 
-    return top_products_list
+    return top_products_list[:5]
